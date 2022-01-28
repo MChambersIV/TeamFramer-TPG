9573ed357ac3f46280fb70df292d53d918e9b89e
MChambersIV <chambersm025@gmail.com>
2022-01-27 19:36:21 -0800

    further index.js tinkering creating html for templating purposes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const inquirer2 = require('inquirer'); 
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
@@ -28,12 +29,19 @@ inquirer
         message: 'What is their office number?',
         name: 'office',
     },
+    {
+        type: 'confirm',
+        message: 'Would like to add another employee?',
+        name: 'choice',
+    }
 ])
 .then((response) => {
-    const {name, id, email, office} = response;
+    const {name, id, email, office, choice} = response;
 
     const employee1 = new Manager(name, id, email, office);
 
+    if (choice === true){
 
+    }
 }
 )
